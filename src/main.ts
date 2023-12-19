2d01ec0eb20bbd70f0a75f3919c24e7334d696c4
Sergi <sergikusakin@gmail.com>
2023-12-19 14:40:44 +0100

    install primevue

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,11 @@
 import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
+import PrimeVue from "primevue/config";
+import "primevue/resources/themes/lara-light-green/theme.css";
 
 const app = createApp(App);
 
+app.use(PrimeVue);
 app.use(router);
-
 app.mount("#app");
