0b77dcf0e886b4a4eca835cfecaeaaeb9b66bd2d
Sergi <sergikusakin@gmail.com>
2023-12-19 16:05:01 +0100

    create notification component

---
+++ src/components/Notifications.vue
@@ -0,0 +1,29 @@
+<template>
+  <f-dropdown :distance="1" placement="right-end" style="display: inline-flex">
+    <!-- This will be the popover reference (for the events and position) -->
+    <button>
+      <Icon :icon="notificationsIcon" />
+    </button>
+
+    <template #popper>
+      <!-- You can put other components too -->
+      <div>1</div>
+      <div>2</div>
+      <div>3</div>
+    </template>
+  </f-dropdown>
+</template>
+
+<script setup lang="ts">
+import { Dropdown as FDropdown } from "floating-vue";
+import { Icon } from "@iconify/vue";
+import notificationsIcon from "@iconify-icons/material-symbols/notifications";
+</script>
+
+<style scoped>
+.dropdown-wrapper {
+  display: flex;
+  justify-items: start;
+  align-items: start;
+}
+</style>
--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,11 @@
 <template>
   <h1>HOME</h1>
   <div>Welcome</div>
+  <Notifications />
 </template>
 
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import Notifications from "../components/Notifications.vue";
+</script>
 
 <style scoped></style>
