3b21104ced547270c933956b99a85a3ff4df0c91
Sergi <sergikusakin@gmail.com>
2023-12-19 15:54:25 +0100

    add styles primevue

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,12 @@ import { createApp } from "vue";
 import App from "./App.vue";
 import router from "./router/router";
 import PrimeVue from "primevue/config";
+import "primevue/resources/primevue.css";
 import "primevue/resources/themes/lara-light-green/theme.css";
+import "floating-vue/dist/style.css";
 
 const app = createApp(App);
 
-app.use(PrimeVue);
 app.use(router);
+app.use(PrimeVue);
 app.mount("#app");
