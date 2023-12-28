046f7648527c5be039b11c5e7e318567a69759fc
Sergi <sergikusakin@gmail.com>
2023-12-29 00:03:45 +0100

    refactor name for import

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -3,7 +3,7 @@
     class="flex flex-col justify-between h-screen pb-5 p-1 w-[65px] border-r border-r-gray-300"
   >
     <div class="flex flex-col justify-center items-center gap-2 pt-8 w-full">
-      <SortList />
+      <OrganizationSelect />
     </div>
     <div class="flex flex-col justify-center items-center gap-2 w-full">
       <Notifications />
@@ -13,7 +13,7 @@
 
 <script setup lang="ts">
 import Notifications from "../components/Notifications.vue";
-import SortList from "../components/OrganizationSelect.vue";
+import OrganizationSelect from "../components/OrganizationSelect.vue";
 </script>
 
 <style scoped></style>
