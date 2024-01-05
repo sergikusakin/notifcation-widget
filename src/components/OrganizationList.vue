10b2578cc11f2f88be68fc93adc24894fc22f04d
Sergi <sergikusakin@gmail.com>
2024-01-05 15:12:34 +0100

    create emit

--- src/components/OrganizationList.vue
+++ src/components/OrganizationList.vue
@@ -1,48 +1,58 @@
 <template>
-  <!-- search -->
-  <div class="relative h-12 p-1 border-b border-b-gray-300">
-    <i-icon
-      class="flex ml-2 items-center justify-center absolute left-1 top-1/2 -translate-y-1/2 pointer-events-none"
-      :icon="searchLine"
-    />
-    <pv-input-text
-      class="w-full pl-8 h-full"
-      v-model="search"
-      placeholder="Search..."
-    />
-  </div>
-  <!-- scroll panel -->
-  <pv-scroll-panel
-    style="width: 100%; height: 500px"
-    class="transition-all duration-500"
-  >
-    <!-- organization info -->
-
-    <div class="inline">
-      <div
-        v-for="item in Array(13)"
-        class="cursor-pointer m-1 p-2 border border-gray-400 rounded flex justify-start items-center"
-      >
-        <pv-button class="gap-2">
-          <img
-            class="flex rounded-full h-10 w-10 object-contain"
-            src="../assets/img/KUSAKINS-favicon.png"
-            alt="#"
-          />
-          <div class="text-sm">Organization name</div>
-        </pv-button>
-      </div>
+  <div class="">
+    <!-- search -->
+    <div class="relative h-12 border-b border-b-gray-300 flex">
+      <i-icon
+        class="flex ml-2 items-center justify-center absolute left-1 top-1/2 -translate-y-1/2 pointer-events-none"
+        :icon="searchLine"
+      />
+      <pv-input-text
+        class="grow pl-8 h-full border-none"
+        v-model="search"
+        placeholder="Search..."
+      />
     </div>
-  </pv-scroll-panel>
+    <!-- scroll panel -->
+    <pv-scroll-panel
+      style="width: 100%; height: 500px"
+      class="transition-all duration-500"
+    >
+      <!-- organization info -->
 
-  <!-- button for add -->
+      <div class="flex flex-col">
+        <div
+          v-for="item in Array(13)"
+          class="cursor-pointer border-b p-2 border-gray-300 flex justify-start items-center hover:bg-gray-100"
+        >
+          <pv-button class="flex gap-2">
+            <img
+              class="flex rounded-full h-10 w-10 object-contain"
+              src="../assets/img/KUSAKINS-favicon.png"
+              alt="#"
+            />
+            <div class="text-sm">Organization name</div>
+          </pv-button>
+        </div>
+      </div>
+    </pv-scroll-panel>
 
-  <pv-button
-    class="flex justify-center h-12 w-[300px] bg-blue-100 rounded-none border-gray-400 text-sm"
-    v-model="create"
-    >+ Organization
-  </pv-button>
+    <!-- button for add -->
+
+    <pv-button
+      class="flex justify-center h-12 w-full bg-blue-100 rounded-none text-sm"
+      @click="$emit('create')"
+      >+ Organization
+    </pv-button>
+  </div>
 </template>
+
+<script lang="ts">
+export type OrganizationOption = {
+  id: string;
+  name: string;
+  imageUrl?: string;
+};
+</script>
 
 <script setup lang="ts">
 import PvButton from "primevue/button";
@@ -54,7 +64,9 @@ import { ref } from "vue";
 
 const search = defineModel<string>("search");
 
-const create = defineEmits<any>("create");
+const emit = defineEmits<{
+  create: [];
+}>();
 
 // const props = defineProps<{
 //   search: string;
